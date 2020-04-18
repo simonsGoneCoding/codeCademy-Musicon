1319da9404e92cc2b8f231adeb5192c14b47fa3d
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-04-18 19:12:59 +0100

    Fully refactored code to use Handlebars. Assinged all shop products to template.

--- files/public/main.js
+++ files/public/main.js
@@ -39,3 +39,13 @@ const context = {
     },
   ],
 };
+
+const templateElement = document.getElementById("templateHB");
+
+const templateSource = templateElement.innerHTML;
+
+const template = Handlebars.compile(templateSource);
+
+const compiledHTML = template(context);
+
+document.getElementById("information").innerHTML = compiledHTML;
